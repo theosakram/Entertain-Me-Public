5d34046608c89bfcd0322f606db1cffa58b3107c
theosakram <wyrdhn@gmail.com>
2020-08-13 19:24:58 +0700

    refactoring congif

--- server/express-app/config/index.js
+++ server/express-app/config/index.js
@@ -3,18 +3,13 @@ const url = "mongodb://localhost:27017";
 const dbName = "entertain-me";
 const client = new MongoClient(url, { useUnifiedTopology: true });
 
-async function connecting() {
-  try {
-    await client.connect();
-    await client.db(dbName).command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } catch (err) {
-    console.log(err);
-    await client.close();
-  }
-}
+const connecting = async () => {
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
+  console.log("Connected successfully to server");
+};
 
-connecting();
+connecting().catch(console.log);
 const db = client.db(dbName);
 
 module.exports = { connecting, db };
