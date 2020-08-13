2eda0846e99ee66643114665fdb194fcd90e2ab1
theosakram <wyrdhn@gmail.com>
2020-08-13 21:51:59 +0700

    minor bug fix

--- server/orchestrator/routes/series.js
+++ server/orchestrator/routes/series.js
@@ -3,8 +3,8 @@ const Controller = require("../controller/movies");
 
 route.get("/", Controller.find);
 route.get("/:id", Controller.findById);
-route.get("/", Controller.add);
-route.get("/:id", Controller.edit);
-route.get("/:id", Controller.remove);
+route.post("/", Controller.add);
+route.put("/:id", Controller.edit);
+route.delete("/:id", Controller.remove);
 
 module.exports = route;
